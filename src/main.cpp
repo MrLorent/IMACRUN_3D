7e32b348d66199cbf72e46b4fedaf16868fd3a24
TLORENT <lorent.tanguy@gmail.com>
2021-11-20 18:25:13 +0100

    CHEVALIER_!!!

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,63 @@
-#include "./App.hpp"
+/** 
+ * Copyright (C) 2018 Tomasz Ga³aj
+ **/
 
-static App& get_app(GLFWwindow* window)
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#define  GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "rendering/Shader.h"
+#include "rendering/Texture.h"
+#include "rendering/Model.h"
+
+GLFWwindow* window;
+const int WINDOW_WIDTH  = 1024;
+const int WINDOW_HEIGHT = 768;
+
+Model   * mesh    = nullptr;
+Shader  * shader  = nullptr;
+Texture * texture = nullptr;
+
+/* Matrices */
+glm::vec3 cam_position = glm::vec3(0.0f, 1.0f, 1.2f);
+glm::vec3 cam_look_at  = glm::vec3(0.0f, 0.5f, 0.0f);
+glm::vec3 cam_up       = glm::vec3(0.0f, 1.0f, 0.0f);
+
+glm::mat4 world_matrix      = glm::mat4(1.0f);
+glm::mat4 view_matrix       = glm::lookAt(cam_position, cam_look_at, cam_up);
+glm::mat4 projection_matrix = glm::perspectiveFov(glm::radians(60.0f), float(WINDOW_WIDTH), float(WINDOW_HEIGHT), 0.1f, 10.0f);
+
+void window_size_callback(GLFWwindow* window, int width, int height)
 {
-    return *reinterpret_cast<App*>(glfwGetWindowUserPointer(window));
+    glViewport(0, 0, width, height);
+    projection_matrix = glm::perspectiveFov(glm::radians(60.0f), float(width), float(height), 0.1f, 10.0f);
+
+    if (shader != nullptr)
+    {
+        shader->setUniformMatrix4fv("viewProj", projection_matrix * view_matrix);
+    }
 }
 
-int main(int argc, char** argv)
+int init()
 {
     /* Initialize the library */
-    if (!glfwInit()) {
+    if (!glfwInit())
         return -1;
-    }
 
     /* Create a windowed mode window and its OpenGL context */
-#ifdef __APPLE__
-    /* We need to explicitly ask for a 3.3 context on Mac */
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#endif
-    GLFWwindow* window = glfwCreateWindow(720, 720, "Hello World", nullptr, nullptr);
-    if (!window) {
+
+    window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Hello Modern GL!", nullptr, nullptr);
+
+    if (!window)
+    {
         glfwTerminate();
         return -1;
     }
@@ -29,37 +65,75 @@ int main(int argc, char** argv)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    /* Intialize glad (loads the OpenGL functions) */
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    glfwSetWindowSizeCallback(window, window_size_callback);
+
+    /* Initialize glad */
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    /* Create the App */
-    int w, h;
-    glfwGetWindowSize(window, &w, &h);
-    App app{w, h, argv[0]};
+    /* Set the viewport */
+    glClearColor(0.6784f, 0.8f, 1.0f, 1.0f);
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 
-    /* Hook user inputs to the App */
-    glfwSetWindowUserPointer(window, reinterpret_cast<void*>(&app));
-    glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods) {
-        get_app(window).key_callback(key, scancode, action, mods);
-    });
-    glfwSetMouseButtonCallback(window, [](GLFWwindow* window, int button, int action, int mods) {
-        get_app(window).mouse_button_callback(button, action, mods);
-    });
-    glfwSetScrollCallback(window, [](GLFWwindow* window, double xoffset, double yoffset) {
-        get_app(window).scroll_callback(xoffset, yoffset);
-    });
-    glfwSetCursorPosCallback(window, [](GLFWwindow* window, double xpos, double ypos) {
-        get_app(window).cursor_position_callback(xpos, ypos);
-    });
-    glfwSetWindowSizeCallback(window, [](GLFWwindow* window, int width, int height) {
-        get_app(window).size_callback(width, height);
-    });
+    glEnable(GL_DEPTH_TEST);
+
+    return true;
+}
+
+int loadContent()
+{
+    mesh = new Model("res/models/alliance.obj");
+
+    /* Create and apply basic shader */
+    shader = new Shader("Basic.vert", "Basic.frag");
+    shader->apply();
+
+    shader->setUniformMatrix4fv("world",        world_matrix);
+    shader->setUniformMatrix3fv("normalMatrix", glm::inverse(glm::transpose(glm::mat3(world_matrix))));
+    shader->setUniformMatrix4fv("viewProj",     projection_matrix * view_matrix);
+
+    shader->setUniform3fv("cam_pos", cam_position);
+
+    texture = new Texture();
+    texture->load("res/models/alliance.png");
+    texture->bind();
+
+    return true;
+}
+
+void render(float time)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    /* Draw our triangle */
+    world_matrix = glm::rotate(glm::mat4(1.0f), time * glm::radians(-90.0f), glm::vec3(0, 1, 0));
+
+    shader->setUniformMatrix4fv("world", world_matrix);
+    shader->setUniformMatrix3fv("normalMatrix", glm::inverse(glm::transpose(glm::mat3(world_matrix))));
+
+    shader->apply();
+    texture->bind();
+    mesh->Draw();
+}
+
+void update()
+{
+    float startTime = static_cast<float>(glfwGetTime());
+    float newTime  = 0.0f;
+    float gameTime = 0.0f;
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window)) {
-        app.render();
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Update game time value */
+        newTime  = static_cast<float>(glfwGetTime());
+        gameTime = newTime - startTime;
+
+        /* Render here */
+        render(gameTime);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -67,9 +141,23 @@ int main(int argc, char** argv)
         /* Poll for and process events */
         glfwPollEvents();
     }
+}
 
-    app.destroy();
+int main(void)
+{
+    if (!init())
+        return -1;
+
+    if (!loadContent())
+        return -1;
+
+    update();
 
     glfwTerminate();
+
+    delete mesh;
+    delete shader;
+    delete texture;
+
     return 0;
 }
