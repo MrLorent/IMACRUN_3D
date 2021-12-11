944964dcaaea9cde0a59a77aa6c9d5530b5b68d1
TLORENT <lorent.tanguy@gmail.com>
2021-12-11 18:42:46 +0100

    [Game] Cleanning

--- src/Game.hpp
+++ src/Game.hpp
@@ -28,7 +28,6 @@ public:
     void initGame();
     void runGame(glm::mat4& projectionMatrix);
 
-    void render(glm::mat4& projectionMatrix);
     void key_callback(int key, int scancode, int action, int mods);
     void mouse_button_callback(int button, int action, int mods);
     void scroll_callback(double xoffset, double yoffset);
