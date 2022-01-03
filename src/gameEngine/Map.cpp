b8d31c935bb76ef2d06e82ccb583f1da1ee87299
TLORENT <lorent.tanguy@gmail.com>
2022-01-03 14:18:08 +0100

    [Map] Cleaning dead code

--- src/gameEngine/Map.cpp
+++ src/gameEngine/Map.cpp
@@ -95,9 +95,6 @@ void Map::initMap()
         _map.push_back(mapStart[i]);
     }
     
-    // for(int i=0; i<_mapElements[0].size() * 2; ++i){
-    //     _map.push_back(_mapElements[0][i%_mapElements[0].size()]);
-    // }
     while(_map.size() < 25 * _mapWidth)
     {
         int randomIndex = _distribution(_random);
